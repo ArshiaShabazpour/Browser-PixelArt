87839ed70b29e39d48d151e29d5d79ac93555079
ArshiaShabazpour <arshiashahbazpour@gmail.com>
2025-07-05 15:21:42 -0400

    added the basic div elements for tools and implemented a rudimentory version of pixel coloring

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 const grids   = document.querySelector("#grids");
 const gridSize = 16;           
 
+let mousedown = false;
+document.body.onmousedown = () => (mousedown = true)
+document.body.onmouseup = () => (mousedown = false)
+const colordiv =document.querySelector("#head");
+const color = colordiv.value;
+
+colordiv.addEventListener("input", (e)=>{e.target.value;})
+
+
+
 function buildGrid(size) {
   grids.innerHTML = "";       
 
   for (let i = 0; i < size * size; i++) {
     const cell = document.createElement("div");
-    cell.style.flex = `0 1 ${100 / size}%`;
+    cell.style.flex = `1 1 ${100 / size}%`;
+    cell.addEventListener("mouseenter",  function (e){ if(mousedown){e.target.style.background = color;}});
     grids.appendChild(cell);
   }
 }

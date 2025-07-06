dcb15a3a14763c44ebdc1fe404db02d6c831377c
ArshiaShabazpour <arshiashahbazpour@gmail.com>
2025-07-05 20:00:06 -0400

    added color changing logic and button activation logic

--- script.js
+++ script.js
@@ -5,10 +5,39 @@ let mousedown = false;
 document.body.onmousedown = () => (mousedown = true)
 document.body.onmouseup = () => (mousedown = false)
 const colordiv =document.querySelector("#head");
-const color = colordiv.value;
 
 colordiv.addEventListener("input", (e)=>{e.target.value;})
 
+const toolButtons = document.querySelectorAll('#tools button');
+let mode = 'color';   
+
+function activateButton(clickedBtn) {
+  toolButtons.forEach(btn => {
+    btn.classList.remove('active');
+    btn.disabled = false;    
+  });
+  clickedBtn.classList.add('active');
+  mode = clickedBtn.id;
+  console.log(mode)
+}
+
+toolButtons.forEach(button => {
+  if(button.id != "reset"){
+  button.addEventListener('click', () => activateButton(button));
+  }
+});
+
+function resetcanvas(){
+  array = document.querySelectorAll('#grids div');
+  array.forEach(cell => {
+    cell.style.background = "white";
+  });
+}
+
+
+
+let resetbutton = document.querySelector("#reset");
+resetbutton.addEventListener('click',resetcanvas);
 
 
 function buildGrid(size) {
@@ -17,7 +46,8 @@ function buildGrid(size) {
   for (let i = 0; i < size * size; i++) {
     const cell = document.createElement("div");
     cell.style.flex = `1 1 ${100 / size}%`;
-    cell.addEventListener("mouseenter",  function (e){ if(mousedown){e.target.style.background = color;}});
+    cell.addEventListener("mouseenter",  function (e){ if(mousedown){e.target.style.background = colordiv.value;}});
+    cell.addEventListener("mousedown",  function (e){e.target.style.background = colordiv.value;});
     grids.appendChild(cell);
   }
 }
